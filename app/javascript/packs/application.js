9869a9e1a99e4c1d50bec5744e5673d07eda33bb
EC2 Default User <ec2-user@ip-172-31-36-77.ec2.internal>
2022-02-03 08:05:59 +0000

    [Add]13章

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "script.js"
+import 'jquery'
+import "scripts/skippr.min"
+import "skippr.min.js"
 
 Rails.start()
 Turbolinks.start()
